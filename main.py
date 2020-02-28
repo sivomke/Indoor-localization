350d95316baa81445ad2f257bca0eb61e7a63fe9
sivomke <ocean1641@gmail.com>
2020-02-28 21:03:39 +0200

    - extracting valuable measurements (3 or more RSSI ) - converting RSSI to distance - creating grid and iBeacons oordinates on the grid

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ df = pd.read_csv('iBeacon_RSSI_Labeled.csv')
 # print(df.tail())
 # df.info()
 # print(df.shape)
-print(df.keys())
+print(df.keys().to_list())
+beacons_name = np.array(df.keys().to_list()[2:])
+
 
 # as we newest measurements are on top
 # as we want to reconstruct trajectory we want to start from earlier measurements
@@ -28,12 +30,14 @@ df.insert(loc=2, column='time', value=time_data)
 
 # list of distinct dates on which the measurements were done
 dates = df['date'].drop_duplicates().to_list()
+print("# of dates: {}".format(len(dates)))
 print(dates)
 
-current_date = dates[5]
+current_date = dates[2]
+print("current date: {}".format(current_date))
 current_df = df[df['date'] == current_date]  # extract date corresponding to current date
 # print(current_df.head())
-print(current_df.shape)
+print("# of time points corresponding to chosen date: {}".format(current_df.shape[0]))
 
 # now we want to extract measurements where we have RSSI from 3 or more iBeacons
 selection = []  # i-th element is true iff row with the same index contains RSSI from 3 or more iBeacons
@@ -45,30 +49,20 @@ for i in range(current_df.shape[0]):
         selection.append(False)
     # print(current_df.iloc[i][3:].to_list())
 
-valid_df = current_df[selection]  # extracted measurements
-print(valid_df.shape)
 
 # we create a 10*10 ft grid, with (0,0) being top-left corner
 
-# approximate coodinates of 13 iBeacons in that grid
+# approximate coordinates of 13 iBeacons in that grid
 beacons_coords = np.array([[80, 50], [30, 100], [30, 140], [30, 180],
                            [65, 100], [65, 40], [65, 180], [95, 100],
                            [145, 40], [145, 100], [145, 100], [145, 180],
                            [145, 220]])
 
-print(valid_df.iloc[0])
-print(valid_df.iloc[0, 0])
+valid_df = current_df[selection]  # extracted measurements
+print("# of valid measurements: {}".format(valid_df.shape[0]))
+# print("1:  ".format(valid_df.iloc[0]))
+# print(valid_df.iloc[0, 0])
 
-# RSSI = np.array(valid_df.iloc[0, 3:])
-RSSI = np.array(valid_df.iloc[2, 3:])
-valid_RSSI = np.array(RSSI[RSSI != -200])
-valid_beacons = np.array(beacons_coords[RSSI != -200])
-
-
-m = valid_beacons.shape[0]
-
-N = 2  # dimension
-eps = pow(10, -8)
 
 # square of Euclidean distance in N-dim space
 
@@ -91,7 +85,6 @@ def RSSI_to_dist(rssi, measured_power=-60, n=1.7):
     # return pow(10, 2*(measured_power - rssi)/(10*n))
     return (pow(10, (measured_power - rssi) / (10 * n)) * 3.2808)**2
 
-valid_dist = [RSSI_to_dist(x) for x in valid_RSSI]
 
 # residual for i-th point
 def r_i(i, x):
@@ -120,7 +113,7 @@ def g(x):
 # custom implementation of Levenberg-Marquardt
 def lm():
     x = np.random.rand(2)
-    print("initial guess: {}".format(x))
+    # print("initial guess: {}".format(x))
     steps = 0  # counts number of iterations until the solution is found
     damping_factor = .001  # regularization
     eye = np.identity(N)
@@ -145,11 +138,6 @@ def lm():
     return x
 
 
-# res_1 = least_squares(g, np.random.rand(2)).x
-res_2 = lm()
-print(res_2)
-
-
 def find_index(x):
     return int(x//10)
 
@@ -165,18 +153,27 @@ def get_cell(coords: np.ndarray) -> str:
     col_coord = coords[1]
     return letters[find_index(col_coord)]+rows[find_index(row_coord)]
 
-print(get_cell(res_2))
+
+for i in range(valid_df.shape[0]):
+    RSSI = np.array(valid_df.iloc[i, 3:])
+    valid_RSSI = np.array(RSSI[RSSI != -200])
+    valid_beacons = np.array(beacons_coords[RSSI != -200])
+    valid_names = beacons_name[RSSI != -200]
+    print("seen by: {}".format(valid_names))
 
 
+    m = valid_beacons.shape[0]
 
+    N = 2  # dimension
+    eps = pow(10, -8)
 
+    valid_dist = [RSSI_to_dist(x) for x in valid_RSSI]
 
+    # res_1 = least_squares(g, np.random.rand(2)).x
+    res_2 = lm()
+    # print(res_2)
 
-
-
-
-
-
+    print("real: {}, calculated: {}".format(valid_df.iloc[i, 0], get_cell(res_2)))
 
 
 
